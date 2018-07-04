b280bf348fe062e1818d0a33377ac01acc561c81
George Sykes <georgesykes86@gmail.com>
2018-07-04 07:22:10 +0100

    Updates the package.json to clean db and rebuild before tests

--- models/TableGenerator.js
+++ models/TableGenerator.js
@@ -1,33 +1,20 @@
 const pg = require('pg')
-const env = require('./config')
-const connectionString = process.env.DEV_DB || env['development'];
+const env = process.env.ENV || 'development'
+const db = require('../config')[env]['dbConnectionString']
 
-(async () => {
-  const client = new pg.Client(connectionString)
-  client.connect()
-  await client
-    .query('CREATE TABLE mail(id SERIAL PRIMARY KEY, mailfrom VARCHAR(60), mailto VARCHAR(60), mailbody VARCHAR(200))', (err, res) => {
-      if (err) {
-        console.log(err)
-      }
-      client.end()
-    })
+const clientTest = new pg.Client(db)
 
-  const connectionStringTest = process.env.TEST_DB || env['test']
-
-  const clientTest = new pg.Client(connectionStringTest)
-  clientTest.connect()
-  await clientTest.query('CREATE TABLE mail(id SERIAL PRIMARY KEY, mailfrom VARCHAR(60), mailto VARCHAR(60), mailbody VARCHAR(200))', (err, res) => {
-    if (err) {
-      console.log(err)
-    } else {
-      clientTest
-        .query("INSERT INTO mail (mailfrom, mailto, mailbody) VALUES ('George', 'Charlene', 'HI'), ('John', 'Igor', 'BYE');", (err, response) => {
-          if (err) {
-            console.log(err)
-          }
-          clientTest.end()
-        })
-    }
-  })
-})()
+clientTest.connect()
+clientTest.query('CREATE TABLE mail(id SERIAL PRIMARY KEY, mailfrom VARCHAR(60), mailto VARCHAR(60), mailbody VARCHAR(200))', (err, res) => {
+  if (err) {
+    console.log(err)
+  } else {
+    clientTest
+      .query("INSERT INTO mail (mailfrom, mailto, mailbody) VALUES ('George', 'Charlene', 'HI'), ('John', 'Igor', 'BYE');", (err, response) => {
+        if (err) {
+          console.log(err)
+        }
+        clientTest.end()
+      })
+  }
+})
