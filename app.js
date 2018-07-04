802e82d914f33635374996cb0448b1954e2f029a
George Sykes <georgesykes86@gmail.com>
2018-07-04 06:46:56 +0100

    Add test for initialiasing server with client

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let POPServer = require('./lib/popServer')
 let pg = require('pg')
-
 const env = require('./config')['production']
-console.log(env)
+
 let dbClient = new pg.Client(env.dbConnectionString)
 let serverName = new POPServer(env.popHost, env.popPort, dbClient)
 serverName.start()
